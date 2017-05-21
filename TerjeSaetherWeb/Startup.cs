2e749e8044ca7b6b324bbb1b982acf931515844e
Terje Sæther <terjesather@gmail.com>
2017-05-21 21:38:46 +0200

    Add project files.

---
+++ TerjeSaetherWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TerjeSaetherWeb.Startup))]
+namespace TerjeSaetherWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
